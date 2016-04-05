70c1029a4e9fd54b8ec216d0fc6a0a30f5ba54fc
Aliaksandr Filimonau <Aliaksandr_Filimonau@epam.com>
2016-04-05 16:19:46 +0300

    git browser. data binding

--- dev/app/src/main/java/com/afilimonov/gitbrowser/fragments/SetUserNameFragment.java
+++ dev/app/src/main/java/com/afilimonov/gitbrowser/fragments/SetUserNameFragment.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 
 import com.afilimonov.gitbrowser.R;
 import com.afilimonov.gitbrowser.model.Repo;
+import com.afilimonov.gitbrowser.model.User;
 import com.afilimonov.gitbrowser.utils.Constants;
 import com.afilimonov.gitbrowser.utils.Logger;
 import com.afilimonov.gitbrowser.utils.Preferences;
@@ -25,103 +26,114 @@ import java.util.List;
  */
 public class SetUserNameFragment extends BaseFragment {
 
-    protected EditText userNameField;
-    protected View loadReposButton;
-    protected View loadReposButtonProgressBar;
+	protected EditText userNameField;
+	protected View loadReposButton;
+	protected View loadReposButtonProgressBar;
 
-    private LoaderCallback loaderCallback;
+	private LoaderCallback loaderCallback;
 
-    public static SetUserNameFragment newInstance() {
-        return new SetUserNameFragment();
-    }
+	private User user;
 
-    public SetUserNameFragment() {
-        super();
-    }
+	public static SetUserNameFragment newInstance() {
+		return new SetUserNameFragment();
+	}
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Logger.d("SetUserNameFragment.onCreateView()");
-        view = inflater.inflate(R.layout.fragment_set_user_name, container, false);
-        initUserNameField();
-        initLoadReposButton();
-        initLoader();
-        return view;
-    }
+	public SetUserNameFragment() {
+		super();
+	}
 
-    private void initUserNameField() {
-        userNameField = (EditText) view.findViewById(R.id.userNameField);
-        String userName = Preferences.getString(Constants.USER_NAME_KEY, null, getContext());
-        if (userName != null) userNameField.setText(userName);
-    }
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		Logger.d("SetUserNameFragment.onCreateView()");
+		initUser();
+		view = inflater.inflate(R.layout.fragment_set_user_name, container, false);
+		ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+		binding.setUser(getUser());
 
-    private void initLoader() {
-        loaderCallback = new LoaderCallback();
-        Bundle bundle = new Bundle();
-        bundle.putString(ReposLoader.USER_NAME_KEY, getContext().getString(R.string.userNameDefault));
-        getLoaderManager().initLoader(LoaderCallback.REPOS_LOADER_ID, bundle, loaderCallback);
-    }
+		initUserNameField();
+		initLoadReposButton();
+		initLoader();
+		return view;
+	}
 
-    protected void initLoadReposButton() {
-        loadReposButtonProgressBar = view.findViewById(R.id.loadReposButtonProgressBar);
-        loadReposButton = view.findViewById(R.id.loadReposButton);
-        loadReposButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userName = userNameField.getText().toString();
-                if (TextUtils.isEmpty(userName)) {
-                    userNameField.setError(getString(R.string.userNameFieldEmptyErrorText));
+	private void initUser() {
+		User user = new User();
+		user.setName(Preferences.getString(Constants.USER_NAME_KEY, getContext().getString(R.string.userNameDefault), getContext()));
+	}
 
-                } else {
-                    loadReposButton.setVisibility(View.INVISIBLE);
-                    loadReposButtonProgressBar.setVisibility(View.VISIBLE);
+	private void initUserNameField() {
+		userNameField = (EditText) view.findViewById(R.id.userNameField);
+		String userName = Preferences.getString(Constants.USER_NAME_KEY, null, getContext());
+		if (userName != null) userNameField.setText(userName);
+	}
 
-                    loadRepos(userName);
-                }
-            }
-        });
-    }
+	private void initLoader() {
+		loaderCallback = new LoaderCallback();
+		Bundle bundle = new Bundle();
+		bundle.putString(ReposLoader.USER_NAME_KEY, getContext().getString(R.string.userNameDefault));
+		getLoaderManager().initLoader(LoaderCallback.REPOS_LOADER_ID, bundle, loaderCallback);
+	}
 
-    private void loadRepos(String enteredUserName) {
-        Loader<List<Repo>> loader = getLoaderManager().getLoader(LoaderCallback.REPOS_LOADER_ID);
-        if (loader == null || !enteredUserName.equals(((ReposLoader) loader).getUserName())) {
-            Bundle bundle = new Bundle();
-            bundle.putString(ReposLoader.USER_NAME_KEY, enteredUserName);
-            loader = getLoaderManager().restartLoader(LoaderCallback.REPOS_LOADER_ID, bundle, loaderCallback);
-        }
-        loader.forceLoad();
-    }
+	protected void initLoadReposButton() {
+		loadReposButtonProgressBar = view.findViewById(R.id.loadReposButtonProgressBar);
+		loadReposButton = view.findViewById(R.id.loadReposButton);
+		loadReposButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String userName = userNameField.getText().toString();
+				if (TextUtils.isEmpty(userName)) {
+					userNameField.setError(getString(R.string.userNameFieldEmptyErrorText));
 
-    private class LoaderCallback implements LoaderManager.LoaderCallbacks<List<Repo>> {
+				} else {
+					loadReposButton.setVisibility(View.INVISIBLE);
+					loadReposButtonProgressBar.setVisibility(View.VISIBLE);
 
-        public static final int REPOS_LOADER_ID = 1;
+					loadRepos(userName);
+				}
+			}
+		});
+	}
 
-        @Override
-        public Loader<List<Repo>> onCreateLoader(int id, Bundle args) {
-            Loader<List<Repo>> loader = null;
-            if (id == REPOS_LOADER_ID) {
-                loader = new ReposLoader(getContext(), args);
-            }
-            return loader;
-        }
+	private void loadRepos(String enteredUserName) {
+		Loader<List<Repo>> loader = getLoaderManager().getLoader(LoaderCallback.REPOS_LOADER_ID);
+		if (loader == null || !enteredUserName.equals(((ReposLoader) loader).getUserName())) {
+			Bundle bundle = new Bundle();
+			bundle.putString(ReposLoader.USER_NAME_KEY, enteredUserName);
+			loader = getLoaderManager().restartLoader(LoaderCallback.REPOS_LOADER_ID, bundle, loaderCallback);
+		}
+		loader.forceLoad();
+	}
 
-        @Override
-        public void onLoadFinished(Loader<List<Repo>> loader, List<Repo> data) {
-            Logger.d("LoaderCallback.onLoadFinished");
-            loadReposButton.setVisibility(View.VISIBLE);
-            loadReposButtonProgressBar.setVisibility(View.GONE);
-            if (data != null) {
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, ReposListFragment.newInstance())
-                        .commitAllowingStateLoss();
-                getLoaderManager().destroyLoader(REPOS_LOADER_ID);
-            }
-        }
+	private class LoaderCallback implements LoaderManager.LoaderCallbacks<List<Repo>> {
 
-        @Override
-        public void onLoaderReset(Loader<List<Repo>> loader) {
-            Logger.d("LoaderCallback.onLoaderReset");
-        }
-    }
+		public static final int REPOS_LOADER_ID = 1;
+
+		@Override
+		public Loader<List<Repo>> onCreateLoader(int id, Bundle args) {
+			Loader<List<Repo>> loader = null;
+			if (id == REPOS_LOADER_ID) {
+				loader = new ReposLoader(getContext(), args);
+			}
+			return loader;
+		}
+
+		@Override
+		public void onLoadFinished(Loader<List<Repo>> loader, List<Repo> data) {
+			Logger.d("LoaderCallback.onLoadFinished");
+			loadReposButton.setVisibility(View.VISIBLE);
+			loadReposButtonProgressBar.setVisibility(View.GONE);
+			if (data != null) {
+				getActivity().getSupportFragmentManager().beginTransaction()
+						.replace(R.id.container, ReposListFragment.newInstance())
+						.commitAllowingStateLoss();
+				getLoaderManager().destroyLoader(REPOS_LOADER_ID);
+			}
+		}
+
+		@Override
+		public void onLoaderReset(Loader<List<Repo>> loader) {
+			Logger.d("LoaderCallback.onLoaderReset");
+		}
+	}
 }
